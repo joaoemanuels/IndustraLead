f4d55e0478e17de78a3d557fb7b5f5049f215eea
joaoemanuels <jemanuel.pi@gmail.com>
2023-10-11 13:51:05 -0300

    .

---
+++ js/script.js
@@ -0,0 +1,10 @@
+import initAnimationScroll from "./modules/animacao-scroll.js";
+import initMenuMobile from "./modules/menu-mobile.js";
+import initMenuTransition from "./modules/menu-transition.js";
+
+
+
+
+initAnimationScroll();
+initMenuTransition();
+initMenuMobile();
